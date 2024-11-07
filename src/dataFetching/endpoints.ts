ff6b1ff22fcd59095ab304ae0f45782ea5d0c47a
javirrof <javirofe96@gmail.com>
2024-11-07 16:24:00 +0100

    Add endpoints for projects and solana price

---
+++ src/dataFetching/endpoints.ts
@@ -0,0 +1,19 @@
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL
+
+export const BASE_URL = `${BACKEND_URL}/api`
+
+export const projectEndpoints = {
+  create: `${BASE_URL}/projects/create`, //POST
+  getProjects: `${BASE_URL}/projects`, //GET
+  getProjectById: (id: string) => `${BASE_URL}/projects/${id}`, //GET
+  getProjectLikesById: (id: string) => `${BASE_URL}/project-likes/${id}`, //GET
+  getProjectCommentsAmountById: (id: string) => `${BASE_URL}/project-comments-amount/${id}`, //GET
+  getProjectCommentsById: (id: string) => `${BASE_URL}/project/comments/${id}`, //GET
+  manageProjectLikes: `${BASE_URL}/projects/manage-like`, //POST
+  addCommentToProject: `${BASE_URL}/projects/add-comment`, //POST
+  deleteCommentFromProject: `${BASE_URL}/projects/comments`, // DELETE
+}
+
+export const priceEndpoints = {
+  getSolanaPrice: `${BASE_URL}/solana`, //GET
+}
