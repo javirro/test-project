086ad382c4372d48564a7f9407f5704a795afd44
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-11-07 09:50:56 +0100

    feat: routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 
 import styles from './page.module.css'
-import TapBar from '@/components/tapBar/TapBar'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <TapBar />
-    </div>
-  )
+  return <div className={styles.page}></div>
 }
