4cb459dbb51f9980fe5574d18c652d0e0830c72e
javirrof <javirofe96@gmail.com>
2024-11-17 08:11:55 +0100

    Add solana balances functions

---
+++ src/contracts/getBalances.ts
@@ -0,0 +1,45 @@
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js'
+import { Account, getAccount, getAssociatedTokenAddress } from '@solana/spl-token'
+import { convertFromLamportToSol } from './lamportSolConverter'
+
+export interface SolanaBalance {
+  lamportSolBalance: string
+  solBalance: string
+}
+
+export interface TokenBalance {
+  lamportTokenBalance: bigint
+  tokenBalance: string
+}
+
+export const getSolanaBalance = async (userWalletAddress: string): Promise<{ lamportSolBalance: string; solBalance: string }> => {
+  const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed')
+  const publicKey = new PublicKey(userWalletAddress)
+  const lamportSolBalance = await connection.getBalance(publicKey)
+  const solBalance = convertFromLamportToSol(lamportSolBalance.toString())
+  return { solBalance, lamportSolBalance: lamportSolBalance.toString() }
+}
+
+
+//* The token accounts are associated with specific tokenMintAddress. These accounts are created and owned by the SPL Token program.
+//* Token accounts are controlled by the user wallet addresses. For that reason, is possible to have several token accounts associated with a single wallet address. 
+export const getTokenBalance = async (userWalletAddress: string, tokenMintAddress: string): Promise<TokenBalance> => {
+  const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed')
+  const walletPublicKey = new PublicKey(userWalletAddress)
+  const tokenMintPublicKey = new PublicKey(tokenMintAddress)
+
+  // Get the associated token account address for the wallet and token
+  const allowOwnerOffCurve = true
+  const tokenAccountAddress = await getAssociatedTokenAddress(tokenMintPublicKey, walletPublicKey, allowOwnerOffCurve)
+  try {
+    const tokenAccountInfo: Account = await getAccount(connection, tokenAccountAddress)
+
+    // Balance is in the smallest unit. In this case all tokens have 9 decimal places.
+    const lamportTokenBalance: bigint = tokenAccountInfo.amount
+    const tokenBalance: string = convertFromLamportToSol(lamportTokenBalance.toString())
+    return { lamportTokenBalance, tokenBalance }
+  } catch (error) {
+    console.error(`Error fetching balance for token ${tokenMintAddress}`, error)
+    return { lamportTokenBalance: BigInt(0), tokenBalance: "0" }
+  }
+}
---
+++ src/contracts/lamportSolConverter.ts
@@ -0,0 +1,23 @@
+import { LAMPORTS_PER_SOL } from '@solana/web3.js'
+
+// Lamport is the smallest unit of SOL. 1 SOL = 10^9 lamports
+// In Solana network tokens have between 0 and 9 decimal places
+
+export const convertFromLamportToSol = (lamportAmount: string): string => {
+  const lamport = parseInt(lamportAmount)
+  return (lamport / LAMPORTS_PER_SOL).toFixed(9)
+}
+
+export const convertFromSolToLamport = (solAmount: string): string => {
+  const sol = parseFloat(solAmount)
+  const value = sol * LAMPORTS_PER_SOL
+  return value.toString()
+}
+
+export const functionalConversionLamportToSol = (lamportAmount: bigint): bigint => {
+  return lamportAmount / BigInt(LAMPORTS_PER_SOL)
+}
+
+export const functionalConversionSolToLamport = (solAmount: bigint): bigint => {
+  return solAmount * BigInt(LAMPORTS_PER_SOL)
+}
