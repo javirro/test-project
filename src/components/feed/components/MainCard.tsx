bfe1a7be0421f5fefa87bdff451e81ba3d893d59
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-11-18 10:56:23 +0100

    Feat: fix project

--- src/components/feed/components/MainCard.tsx
+++ src/components/feed/components/MainCard.tsx
@@ -58,7 +58,7 @@ function MainCard() {
   )
 
   const onHandleClick = () => {
-    router.push('/token-details/jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL')
+    router.push('/token-details/jtojtomepa8beP8AuQc6eXt5FriJwfFMwQx2v2f9mCL/overview')
   }
 
   return (
