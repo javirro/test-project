d7d984bdcd60cd29febee45217178d4f40a9e1fe
javirrof <javirofe96@gmail.com>
2024-11-19 17:51:14 +0100

    update next.js to version 15.0.3 and remove minification

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(webpackConfig) {
+    return {
+      ...webpackConfig,
+      optimization: {
+        minimize: false,
+      },
+    }
+  },
+}
 
-export default nextConfig;
+export default nextConfig
