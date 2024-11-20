10b48de5ac2e0500d292107f9aa7aff4693f4d4a
javirrof <javirofe96@gmail.com>
2024-11-20 09:53:47 +0100

    Add commets for everything in overview

--- src/app/token-details/[tokenAddress]/overview/page.tsx
+++ src/app/token-details/[tokenAddress]/overview/page.tsx
@@ -1,10 +1,10 @@
 import TokenDetailsNavBar from '@/components/navigation/tokenDetailsNavBar/TokenDetailsNavBar'
 import style from './page.module.css'
-import MarketInfoContainer from '../../components/marketInfoContainer/MarketInfoContainer'
+// import MarketInfoContainer from '../../components/marketInfoContainer/MarketInfoContainer'
 // import CommentsContainer from '../../components/commentsContainer/CommentsContainer'
 // import CreatedBy from '../../components/createdBy/CreatedBy'
-import TagsContainer from '../../components/tagsContainer/TagsContainer'
-import TokenDetails from '../../components/tokenDetails/TokenDetails'
+// import TagsContainer from '../../components/tagsContainer/TagsContainer'
+// import TokenDetails from '../../components/tokenDetails/TokenDetails'
 import { Suspense } from 'react'
 import { getProjectByTokenAddress } from '@/dataFetching/projects/getProject'
 
@@ -26,14 +26,14 @@ async function page({ params }: PageProps) {
 const Overview = async ({ tokenAddress }: { tokenAddress: string }) => {
   const projectInfo = await getProjectByTokenAddress(tokenAddress)
   const { tags, creationDate, creatorAddress } = projectInfo
-  console.log('Overview -> projectInfo', projectInfo, creationDate, creatorAddress)
+  console.log('Overview -> projectInfo', projectInfo, creationDate, creatorAddress, tags)
   return (
     <section className={style.main}>
       <TokenDetailsNavBar />
-      <TokenDetails project={projectInfo} />
+      {/* <TokenDetails project={projectInfo} />
       <MarketInfoContainer project={projectInfo} />
       <TagsContainer tags={tags} />
-      {/* <CommentsContainer tokenAddress={tokenAddress} />
+      <CommentsContainer tokenAddress={tokenAddress} />
       <CreatedBy createdOn={creationDate} creatorAddress={creatorAddress} /> */}
     </section>
   )
