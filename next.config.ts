43fe5332feb7fada25f7f15c62f21c49db23226a
javirrof <javirofe96@gmail.com>
2024-11-21 15:02:45 +0100

    remove unnecesary config for next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/token-details/:tokenAddress/overview',
-        destination: '/token-details/[tokenAddress]/overview',
-      },
-    ];
-  },
 }
 
 export default nextConfig
