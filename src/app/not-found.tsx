39390ecdb07a53b3db11256f63760e338676c9dd
javirrof <javirofe96@gmail.com>
2024-11-24 10:30:10 +0100

    Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+//TODO: Add a 404 page custom style
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
+}
