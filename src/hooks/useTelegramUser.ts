70384ca7deadb77510f4dae6fe094b15a1a2b610
javirrof <javirofe96@gmail.com>
2024-11-26 06:47:50 +0100

    #17: Add userStore to save user information when session is active

--- src/hooks/useTelegramUser.ts
+++ src/hooks/useTelegramUser.ts
@@ -1,19 +1,27 @@
-import { TelegramUser } from '@/types/user'
+import { createOrLogin } from '@/dataFetching/users/createOrLogin'
+import { TelegramUser, User } from '@/types/user'
 import WebApp from '@twa-dev/sdk'
 import { useEffect, useState } from 'react'
+import { useUserStore } from '@/app/store/userStore'
 
 const useTelegramUser = (): TelegramUser | null => {
-  const [user, setUser] = useState<TelegramUser | null>(null)
-  console.log({ TelegramUser: user })
+  const [userTelegramInfo, setUserTelegramInfo] = useState<TelegramUser | null>(null)
+  const { setUser: setUserStore, setToken } = useUserStore()
 
   useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      const userInfo = WebApp.initDataUnsafe.user as TelegramUser
-      setUser(userInfo)
+    const createUser = async () => {
+      if (WebApp.initDataUnsafe.user) {
+        const userInfo = WebApp.initDataUnsafe.user as TelegramUser
+        setUserTelegramInfo(userInfo)
+        const { user: completeUser, token } = await createOrLogin(userInfo)
+        setUserStore(completeUser as User)
+        setToken(token)
+      }
     }
-  }, [])
+    createUser()
+  }, [setUserTelegramInfo, setUserStore, setToken])
 
-  return user
+  return userTelegramInfo
 }
 
 export default useTelegramUser
