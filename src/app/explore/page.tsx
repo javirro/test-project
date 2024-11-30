dbeea8fbbb264326abacaafecc81463d41430c70
javirrof <javirofe96@gmail.com>
2024-11-30 18:48:38 +0100

    Add explore route to use cookies to test

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,23 @@
+import { cookies } from 'next/headers'
+import { Suspense } from 'react'
+
+const ExplorePage = async () => {
+  const cookiesStore = await cookies()
+  console.log("Cookies: ", cookiesStore)
+  return (
+    <div>
+      <h1>Explore</h1>
+    </div>
+  )
+}
+
+
+const ExplorePageWrapper = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+    <ExplorePage />
+    </Suspense>
+  )
+}
+
+export default ExplorePageWrapper
