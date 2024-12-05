1e8e07787dcb817d978e52ce51f897997efe8a2e
javirrof <javirofe96@gmail.com>
2024-12-05 07:51:05 +0100

    #9: Add function to getTransactions list

---
+++ src/dataFetching/transactions/getTransactionsList.ts
@@ -0,0 +1,20 @@
+import { TransactionData } from '@/types/transactions'
+import { User } from '@/types/user'
+import { transactionsEndpoints } from '../endpoints'
+
+export const getTransactionsList = async (user: User, token: string): Promise<TransactionData[]> => {
+  const url = transactionsEndpoints.getTransactionsByUserAddress(user.address)
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `${token}`,
+      'telegram-id': `${user.telegramId}`,
+    },
+  })
+  if (!response.ok) {
+    throw new Error('Error getting transactions')
+  }
+  const { transactions }: { transactions: TransactionData[] } = await response.json()
+  return transactions
+}
