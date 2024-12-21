161affb3c51cd52fd40df3d222b0fbba294be55d
javirrof <javirofe96@gmail.com>
2024-12-21 08:06:02 +0100

    Add solana balance wallet

--- src/app/wallet/[username]/components/Asset/AssetsList.tsx
+++ src/app/wallet/[username]/components/Asset/AssetsList.tsx
@@ -1,15 +1,27 @@
 import { AssetsListProps } from '@/types/assetsList'
 import AssetItem from './AssetItem'
 
-function AssetsList({ assets, username }: AssetsListProps) {
+function AssetsList({ assets, username, solBalance, solPrice }: AssetsListProps) {
+  const addSolanaToAssets = [
+    {
+      name: 'Solana',
+      symbol: 'SOL',
+      address: 'solana',
+      amount: +parseFloat(solBalance)?.toFixed(4),
+      gains: 0,
+      amountInUSD: +(parseFloat(solBalance) * solPrice)?.toFixed(2),
+      image: '/images/solana.png',
+    },
+    ...assets,
+  ]
+
+  const filteredAssetBalanceZero = addSolanaToAssets?.filter((asset) => asset?.amount > 0)
   return (
     <>
-      {assets.map((asset, index) => (
+      {filteredAssetBalanceZero.map((asset, index) => (
         <AssetItem key={index} asset={asset} username={username} />
       ))}
-      <div style={{height: '10vh'}}>
-
-      </div>
+      <div style={{ height: '10vh' }}></div>
     </>
   )
 }
