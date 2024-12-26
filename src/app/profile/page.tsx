a3a026e083c23baf3c3dc19a70327303bfbfaf58
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-12-26 08:36:08 +0100

    Feat: profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,12 @@
+import ProfileHeader from './components/profileHeader/ProfileHeader'
+import style from './page.module.css'
+
 function page() {
-  return <></>
+  return (
+    <main className={style.main}>
+      <ProfileHeader />
+    </main>
+  )
 }
 
 export default page
